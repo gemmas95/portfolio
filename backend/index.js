7c566022ae7022d3f290b6c7941c905f26143a57
gemmas95 <gemma1030@gmail.com>
2020-11-06 14:37:46 +0100

    viewing remote origin

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 const emailRouter = require("./src/routes/emailRouter")();
 
 app.get("/api", (req, res) => {
-  res.send("API Status: Running Successfully!");
+  res.send("API Status: Running and deploying!");
 });
 
 app.use("/api/email", emailRouter);
